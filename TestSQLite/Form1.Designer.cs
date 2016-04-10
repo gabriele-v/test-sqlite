6238b3b357484cd9c1b1599f3d23b6b31f1cdd13
Gabriele-V <gabriele@villanet.it>
2016-04-10 19:52:14 +0200

    First commit

---
+++ TestSQLite/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestSQLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnEncryptRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(13, 13);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(135, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create DB";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(13, 42);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(135, 23);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnEncryptRead
+            // 
+            this.btnEncryptRead.Location = new System.Drawing.Point(13, 71);
+            this.btnEncryptRead.Name = "btnEncryptRead";
+            this.btnEncryptRead.Size = new System.Drawing.Size(135, 23);
+            this.btnEncryptRead.TabIndex = 2;
+            this.btnEncryptRead.Text = "Encrypt Read";
+            this.btnEncryptRead.UseVisualStyleBackColor = true;
+            this.btnEncryptRead.Click += new System.EventHandler(this.EncryptRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(159, 103);
+            this.Controls.Add(this.btnEncryptRead);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnEncryptRead;
+    }
+}
+
---
+++ TestSQLite/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SQLite;
+using SQLitePCL;
+
+namespace TestSQLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Test
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            [MaxLength(8)]
+            public string TestText { get; set; }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            SQLite3Plugin.Init();
+            SQLitePCL.ISQLite3Provider imp = new SQLitePCL.SQLite3Provider_sqlcipher();
+            SQLitePCL.raw.SetProvider(imp);
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "UnEncrypted DB File|*.udb";
+            saveFileDialog1.Title = "Save UnEncrypted DB";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName == "")
+                return;
+
+            var db = new SQLiteConnection(saveFileDialog1.FileName);
+            db.CreateTable<Test>();
+
+            db.Insert(new Test()
+            {
+                TestText = DateTime.Now.ToString()
+            });
+
+            var query = db.Table<Test>();
+
+            foreach (var test in query)
+                Console.WriteLine("Test: " + test.TestText);
+            db.Dispose();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "UnEncrypted DB File|*.udb";
+            openFileDialog1.Title = "Open UnEncrypted DB";
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName == "")
+                return;
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Encrypted DB File|*.edb";
+            saveFileDialog1.Title = "Save Encrypted DB";
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName == "")
+                return;
+
+            SQLite3Plugin.Init();
+            SQLitePCL.ISQLite3Provider imp = new SQLitePCL.SQLite3Provider_sqlcipher();
+            SQLitePCL.raw.SetProvider(imp);
+
+            sqlite3 db;
+            raw.sqlite3_open(openFileDialog1.FileName, out db);
+            raw.sqlite3_exec(db, "ATTACH DATABASE '"+ saveFileDialog1.FileName + "' AS encrypted KEY 'testkey';");
+            raw.sqlite3_exec(db, "SELECT sqlcipher_export('encrypted');");
+            raw.sqlite3_exec(db, "DETACH DATABASE encrypted;");
+            db.Dispose();
+        }
+
+        private void EncryptRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Encrypted DB File|*.edb";
+            openFileDialog1.Title = "Open Encrypted DB";
+            openFileDialog1.ShowDialog();
+
+            if (openFileDialog1.FileName == "")
+                return;
+
+            SQLite3Plugin.Init();
+            SQLitePCL.ISQLite3Provider imp = new SQLitePCL.SQLite3Provider_sqlcipher();
+            SQLitePCL.raw.SetProvider(imp);
+            var db = new SQLiteConnection(openFileDialog1.FileName);
+            db.Execute("PRAGMA key = 'testkey';");
+            var query = db.Table<Test>();
+            foreach (var test in query)
+                MessageBox.Show(test.TestText);
+        }
+    }
+}
